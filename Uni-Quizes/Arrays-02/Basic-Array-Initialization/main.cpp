725dd013e713bd4209c630dce801d279d40ac322
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 04:17:36 +0500

    Create main.cpp

---
+++ Uni-Quizes/Arrays-02/Basic-Array-Initialization/main.cpp
@@ -0,0 +1,18 @@
+// code no 1
+#include <iostream>
+using namespace std;
+
+const int K;
+const int SIZE;
+int main()
+{
+    string friends[5];
+    friends[0] = "Ali";
+    friends[1] = "fahim";
+    friends[2] = "fahim";
+    for (int i = 0; i < 3; i++)
+    {
+        cout << friends[i] << endl;
+    }
+    return 0;
+}
