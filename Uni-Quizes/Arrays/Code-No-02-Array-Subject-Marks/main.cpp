4e573fd94dc0fe4a89087c1cd9e22680eec25d5a
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 04:06:59 +0500

    Update main.cpp

--- Uni-Quizes/Arrays/Code-No-02-Array-Subject-Marks/main.cpp
+++ Uni-Quizes/Arrays/Code-No-02-Array-Subject-Marks/main.cpp
@@ -1,1 +1,17 @@
+// Code No. 02
+#include <iostream>
+using namespace std;
 
+int main() {
+    int marks[] = {60, 70, 72, 69, 90, 40};
+
+    cout << "Your DMC" << endl;
+    cout << "Urdu: " << marks[0] << endl;
+    cout << "Maths : " << marks[1] << endl;
+    cout << "English: " << marks[2] << endl;
+    cout << "Science: " << marks[3] << endl;
+    cout << "Islamiyat: " << marks[4] << endl;
+    cout << "Programming: " << marks[5] << endl;
+
+    return 0;
+}
