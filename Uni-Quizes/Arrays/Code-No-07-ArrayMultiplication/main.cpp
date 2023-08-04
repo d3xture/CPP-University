16079767090efa59bdc078f8352f55312bccfbfa
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 04:12:10 +0500

    Create main.cpp

---
+++ Uni-Quizes/Arrays/Code-No-07-ArrayMultiplication/main.cpp
@@ -0,0 +1,16 @@
+// CODE NO 07
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k = 5;
+    int nos[] = {12, 4, 5, 9, 7};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << nos[i] * k << "\t";
+    }
+    cout << endl;
+    cout << "----------------------------------" << endl;
+}
