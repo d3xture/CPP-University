5b71fa304187444283a15d6542ec33188cc72ea9
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 04:13:00 +0500

    Create main.cpp

---
+++ Uni-Quizes/Arrays/Code-No-08-InputNumbersLooped/main.cpp
@@ -0,0 +1,18 @@
+// code no 08  *Repetition*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nos[6] = {12, 4, 5, 9, 7};
+
+    cout << "Enter 6 numbers: " << endl;
+    cin >> nos[0];
+    cin >> nos[1];
+    cin >> nos[2];
+    cin >> nos[3];
+    cin >> nos[4];
+    cin >> nos[5];
+
+    return 0;
+}
