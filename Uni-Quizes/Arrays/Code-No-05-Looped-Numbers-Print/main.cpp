81f66a023e1c2eeaa1a9c3b6c3427988a90c90a2
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 04:11:08 +0500

    Create main.cpp

---
+++ Uni-Quizes/Arrays/Code-No-05-Looped-Numbers-Print/main.cpp
@@ -0,0 +1,12 @@
+//CODE no 05
+#include <iostream>
+using namespace std;
+int main(){
+int nos [] = {1,2,5,6,9,7};
+cout << "Printing all the numbers: " << endl;
+for(int i = 0; i < 6; i++){
+    cout << nos[i] << endl;
+
+}
+return 0;
+}
