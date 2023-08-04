9c1b5022db686c422674d7fc5f895b4c0d4d8158
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 04:11:38 +0500

    Create main.cpp

---
+++ Uni-Quizes/Arrays/Code-No-06-Array-Looped-Numbers/main.cpp
@@ -0,0 +1,16 @@
+//CODE NO 06
+ #include <iostream>
+ using namespace std;
+
+int main()
+{
+    int nos[] = {12, 4, 5, 9, 7};
+
+    for (int i = 0; i < 5; i++) {
+        cout << nos[i] << "\t";
+    }
+    cout << endl;
+    cout << "----------------------------------" << endl;
+
+    return 0;
+}
