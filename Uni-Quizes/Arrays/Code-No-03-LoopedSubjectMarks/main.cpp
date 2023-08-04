b3e8a5c45db95c5bcf34353a8931d99147221c93
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 04:09:31 +0500

    Create main.cpp

---
+++ Uni-Quizes/Arrays/Code-No-03-LoopedSubjectMarks/main.cpp
@@ -0,0 +1,12 @@
+// Code No. 03
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[] = {60, 70, 72, 69, 90, 40};
+
+    for (int i = 0; i < 6; i++) {
+        cout << "Marks: " << marks[i] << endl;
+    }
+
+    return 0;
