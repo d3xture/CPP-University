648d2d97250858ffce62c664f56e6871f822d995
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 03:37:12 +0500

    Create main.cpp

---
+++ if-else-statements/Vowel-Consonant-Identifier/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch = 'c';
+
+    cout << "Enter the letter: ";
+    cin >> ch;
+
+    if (ch == 'a')
+    {
+        cout << "This is a vowel";
+    }
+    else if (ch == 'e')
+    {
+        cout << "This is a vowel";
+    }
+    else if (ch == 'i')
+    {
+        cout << "This is a vowel";
+    }
+    else if (ch == 'o')
+    {
+        cout << "This is a vowel";
+    }
+    else if (ch == 'u')
+    {
+        cout << "This is a vowel";
+    }
+    else
+        cout << "This is a consonant";
+}
