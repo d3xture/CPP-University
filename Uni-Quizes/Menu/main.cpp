acf9737ce5705ce9bfc1bce517e430814337487f
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 03:59:22 +0500

    Create main.cpp

---
+++ Uni-Quizes/Menu/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    cout << "TODAY'S Menu" << endl;
+    cout << "************" << endl;
+    cout << "Daal Mash" << endl;
+    cout << "Daal Channa" << endl;
+    cout << "Lobia" << endl;
+    goto skip;
+    cout << "Chaat Fruit" << endl;
+    cout << "Ice Cream" << endl;
+skip:
+    cout<<"Pulao";
+    return 0;
+}
