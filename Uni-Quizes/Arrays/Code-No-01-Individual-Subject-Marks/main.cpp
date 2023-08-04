a82b35774bdc00c89e5e10f32c94e14190be2cd4
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 04:05:03 +0500

    Create main.cpp

---
+++ Uni-Quizes/Arrays/Code-No-01-Individual-Subject-Marks/main.cpp
@@ -0,0 +1,22 @@
+// Code No. 01
+#include <iostream>
+using namespace std;
+
+int main() {
+    int urdu = 60;
+    int maths = 70;
+    int english = 72;
+    int science = 69;
+    int islamiyat = 90;
+    int programming = 40;
+
+    cout << "Your DMC" << endl;
+    cout << "Urdu: " << urdu << endl;
+    cout << "Maths: " << maths << endl;
+    cout << "English: " << english << endl;
+    cout << "Science: " << science << endl;
+    cout << "Islamiyat: " << islamiyat << endl;
+    cout << "Programming: " << programming << endl;
+
+    return 0;
+}
