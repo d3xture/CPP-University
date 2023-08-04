dda7a15de2775fcc3fbcd2d3d63e3e3608f138ff
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 04:10:25 +0500

    Create main.cpp

---
+++ Uni-Quizes/Arrays/Code-No-04-Print-Individual-Numbers/main.cpp
@@ -0,0 +1,16 @@
+// Code no 04
+ #include <iostream>
+ using namespace std;
+ int main()
+ {
+     int nos[] = {1, 2, 5, 6, 9, 7};
+     cout << "Printing all numbers: " << endl;
+     cout << nos[0]<<endl;
+     cout << nos[1]<<endl;
+     cout << nos[2]<<endl;
+     cout << nos[3]<<endl;
+     cout << nos[4]<<endl;
+     cout << nos[5]<<endl;
+
+return 0;
+}
