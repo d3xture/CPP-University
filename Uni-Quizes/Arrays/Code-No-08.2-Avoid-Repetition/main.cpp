31619dde9860af0353936817e8addf9ec05f77a1
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 04:13:40 +0500

    Create main.cpp

---
+++ Uni-Quizes/Arrays/Code-No-08.2-Avoid-Repetition/main.cpp
@@ -0,0 +1,16 @@
+//Code no 08 *To Avoid Repetition*
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nos[6] = {12, 4, 5, 9, 7};
+    cout << "Enter 6 numbers: " << endl;
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> nos[i];
+    }
+
+    return 0;
+}
